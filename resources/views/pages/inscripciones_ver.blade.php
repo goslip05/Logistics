73da3a26a739e1515f61eec7c0e154111500a1cb
Diego Jimenez <71677761+goslip05@users.noreply.github.com>
2023-06-02 14:43:50 -0500

    update view

--- resources/views/pages/inscripciones_ver.blade.php
+++ resources/views/pages/inscripciones_ver.blade.php
@@ -169,13 +169,25 @@
                 <h3 class="text-primary">Visualización Clientes Inscritos</h3>
                 <div class="col-sm-6 col-xl-3">
                     <div class="rounded d-flex align-items-center justify-content-between">
-                        <div class="ms-3">
-                            <p class="mb-2">Inscripciones</p>
-                            <h6 class="mb-0">{{$inscripciones->count()}}</h6>
-                        </div>
+                        
                     </div>
                 </div>
                 
+            </div>
+            <div class="card border-left-info shadow h-100 py-2 ml-5">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1" style="margin-right: 90px; font-size: smaller">
+                                Inscripciones:</div>
+                                <p class="text-left ml-1 mt-3"><strong>{{$inscripciones->count()}}</strong></p>                                     
+                        </div>
+                        <div class="col-auto">
+                            
+                        </div>
+                    </div>
+                    
+                </div>
             </div>
         </div>
         <!-- Sale & Revenue End -->
